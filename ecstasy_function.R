cfbe94de7df18a35c141fec78cd39605313f660c
frankchen07 <frankchen07@gmail.com>
2014-11-11 11:50:30 -0500

    cleaner script of ecstasy, for one year

--- ecstasy_function.R
+++ ecstasy_function.R
@@ -1,64 +1,76 @@
 library("XML")
 library("reshape2")
 library("plyr")
+library("data.table")
+
+# Functions
 
 year.params.url <- function(year.start, year.end) {
-	directory.name <- sprintf("http://www.ecstasydata.org/results_xml.php?sold_as_ecstasy=yes&Y1=%s&Y2=%s&max=500", year.start, year.end)
-	return(directory.name)
+	# stores directory name search parameters for specified year
+	dir.name <- sprintf("http://www.ecstasydata.org/results_xml.php?sold_as_ecstasy=yes&Y1=%s&Y2=%s&max=500", year.start, year.end)
+	return(dir.name)
 }
+year.2013.2014 <- year.params.url(2013, 2014)
 
 xml.to.raw <- function(year.params.url) {
-	ecstasy.raw <- xmlToList(xmlParse(year.params.url))
-	return(ecstasy.raw)
+	# creates raw xml data
+	raw <- xmlToList(xmlParse(year.params.url))
+	return(raw)
 }
-
-create.id.actives <- function(xml.to.raw) {
-	for (i in 1:length(xml.to.raw)) { 
-		xml.to.raw[i]$tablet$actives <- as.list(xml.to.raw[i]$tablet$actives)
-	}
-	id.actives.base <- data.frame(xml.to.raw[1]$tablet$id, as.list(xml.to.raw[1]$tablet$actives))
-	colnames(id.actives.base) <- c("id", "class")
-	id.actives.base <- melt(id.actives.base, id.vars="id")
-	
-	for (i in 2:length(xml.to.raw)) {
-		id.actives.add <- data.frame(xml.to.raw[i]$tablet$id, as.list(xml.to.raw[i]$tablet$actives))
-		colnames(id.actives.add) <- c("id", "class")
-		id.actives.add <- melt(id.actives.add, id.vars="id")
-		id.actives.base <- rbind(id.actives.base, id.actives.add)
-	}
-	return(id.actives.base)
-}
-
-create.id.attributes <- function(xml.to.raw) {
-	id.attrib.base <- list(xml.to.raw[1]$tablet$id, 
-						xml.to.raw[1]$tablet$name, 
-						xml.to.raw[1]$tablet$location, 
-						xml.to.raw[1]$tablet$mass)
-	id.attrib.base <- as.data.frame(t(id.attrib.base))
-	
-	for (i in 2:length(xml.to.raw)) {
-		id.attrib.add <- list(xml.to.raw[i]$tablet$id, 
-								xml.to.raw[i]$tablet$name, 
-								xml.to.raw[i]$tablet$location, 
-								xml.to.raw[i]$tablet$mass)
-		id.attrib.add <- as.data.frame(t(id.attrib.add))
-		id.attrib.base <- rbind(id.attrib.base, id.attrib.add)
-	}
-	return(id.attrib.base)
-}
-
-clean.up.finish <- function(id.actives.base, id.attrib.base) {
-	colnames(id.attrib.base) <- c("id", "name", "location", "mass")
-	ecstasy <- join(id.actives.base, id.attrib.base, by = "id", type = "left")
-	ecstasy[, 2:3] <- colsplit(ecstasy[, 3], ":", c("element", "proportion"))
-	colnames(ecstasy) <- c("id", "chemical", "ratio", "name", "location", "mass")
-	return(ecstasy)
-}
-
-#########################################
-
-year.2013.2014 <- year.params.url(2013, 2014)
 ecstasy.raw <- xml.to.raw(year.2013.2014)
-base.table <- create.id.actives(ecstasy.raw)
-supplement.table <- create.id.attributes(ecstasy.raw)
-ecstasy <- clean.up.finish(base.table, supplement.table)
+
+create.id.actives <- function(raw.data) {
+	# stores actives in one row, prepare for melt
+	return(data.frame(raw.data$tablet$id, raw.data$tablet$actives))	
+}
+
+create.id.attributes <- function(raw.data) {
+	return(data.frame(raw.data$tablet$id, 
+					  raw.data$tablet$name,
+				      raw.data$tablet$location,
+					  raw.data$tablet$mass))
+}
+
+melt.data <- function(list.data) {
+	list.data <- rbindlist(list.data, fill=TRUE)
+	list.data <- melt(list.data, id.vars="raw.data.tablet.id")
+	list.data <- list.data[with(list.data, order(raw.data.tablet.id)), ]
+	list.data <- as.data.frame(list.data)
+	return(list.data)
+}
+	 
+###################################################
+###################################################
+###################################################
+###################################################
+
+# ID Actives Processing
+
+# test <- lapply(ecstasy.raw, create.id.actives)
+id.actives <- list()
+for (i in seq(ecstasy.raw)) {
+	id.actives[[i]] <- create.id.actives(ecstasy.raw[i])
+}
+id.actives <- melt.data(id.actives)
+id.actives <- id.actives[, -2]
+id.actives <- id.actives[complete.cases(id.actives), ]
+setnames(id.actives, 1:2, c("id", "composition"))
+id.actives[, 2:3] <- colsplit(id.actives$composition, ":", c("composition", "proportion"))
+
+# ID Attributes Processing
+
+# error handling, NULL values break function create.id.attributes
+id.attributes <- lapply(ecstasy.raw, function(raw.data) if (raw.data$tablet$location == NULL) { raw.data$tablet$location <- "N/A"})
+
+# test <- lapply(ecstasy.raw, create.id.attributes)
+id.attributes <- list()
+for (i in seq(ecstasy.raw)) {
+	id.attributes[[i]] <- create.id.attributes(ecstasy.raw[i])
+}
+id.attributes <- as.data.frame(rbindlist(id.attributes, fill=TRUE))
+setnames(id.attributes, 1:4, c("id", "name", "location", "mass"))
+
+# Final Join 
+ecstasy <- join(id.actives, id.attributes, by = "id", type = "left")
+
+
